3b22a60b50a8157a446b8a3d016349782320ef2c
高永昭 <498763684@qq.com>
2022-03-24 22:19:59 +0800

    dfsfsd

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,5 @@
   <!--一级路由 -->
   <router-view/>
   <div>sdfsdfas</div>
-  <div>fassdfdsfsafsdfsdaf</div>
-  <div>fadsfsfsdaf</div>
-  <div>fsdfsffsdfsdaf</div>
-  <div>fasdsdfsfsdaf</div>
+  sdfsfs
 </template>
